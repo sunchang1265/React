07b6ade6923aef88f9e359a2ea03dfb5551677d0
Chang Sun <sunchang1265@gmail.com>
2018-03-14 23:18:27 -0400

    Event handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Welcome from './welcome';
-import Clock from './clock';
 
 const App = ()=>{
     return (
@@ -14,5 +13,4 @@ const App = ()=>{
 
 export default App;
 
-ReactDOM.render(<App />, document.getElementById("app"));
-ReactDOM.render(<Clock />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("app"));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Welcome from './welcome';
+import Clock from './clock';
+import Toggle from './Toggle';
 import App from './App.js';
 
 const arr = [1, 2, 3];
 const test = () => console.log(...arr);
 window.test = test;
+
+ReactDOM.render(
+    <div>
+        <App />
+        <Clock />
+        <Toggle/>
+    </div>, 
+    document.getElementById("root")
+);
